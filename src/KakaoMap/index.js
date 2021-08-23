638271c708072d3c80e0a674d909f0d95538326b
오종택 <jongtaek@ojongtaeg-ui-MacBookPro.local>
2021-08-23 14:48:41 +0900

    add: react component to kakao custom overlay

--- src/KakaoMap/index.js
+++ src/KakaoMap/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ReactDOMServer from "react-dom/server";
 
 const APP_KEY = "4cbcbc2e9b2ca1cfca0760f5259bd304";
 const divBtnOpt = {
@@ -17,7 +18,7 @@ const App = () => {
   const getLocation = async () => {
     const data = await fetch("/mapMarker.json");
     const dataJSON = await data.json();
-    const arr = dataJSON.homes.map(el => {
+    const arr = dataJSON.homes.map((el) => {
       return {
         home_region: el.home_region,
         home_latitude: el.home_latitude,
@@ -47,19 +48,7 @@ const App = () => {
     };
   };
 
-  const createMarker = () => {
-    const { kakao } = window;
-    const markers = locationArr.map(e => {
-      return new kakao.maps.Marker({
-        map,
-        position: new kakao.maps.LatLng(e.home_latitude, e.home_longitutde),
-      });
-    });
-
-    setMarkerArr(markers);
-  };
-
-  const deleteMarker = () => markerArr.forEach(e => e.setMap(null));
+  const deleteMarker = () => markerArr.forEach((e) => e.setMap(null));
 
   useEffect(() => {
     getLocation(); // location 정보 fetch
@@ -69,6 +58,39 @@ const App = () => {
   // marker 생성 + 표시
   useEffect(() => {
     if (map && locationArr.length) {
+      const createMarker = () => {
+        const { kakao } = window;
+        const markers = locationArr.map((e) => {
+          const position = new kakao.maps.LatLng(
+            e.home_latitude,
+            e.home_longitutde
+          );
+
+          const marker = new kakao.maps.Marker({
+            map,
+            position,
+          });
+
+          const ReactComponent = OverlayComponent();
+          const content = ReactDOMServer.renderToString(ReactComponent);
+
+          const overlay = new kakao.maps.CustomOverlay({
+            position,
+            content,
+          });
+
+          kakao.maps.event.addListener(marker, "click", () => {
+            console.log("click");
+          });
+
+          overlay.setMap(map);
+
+          return marker;
+        });
+
+        setMarkerArr(markers);
+      };
+
       console.log(map, locationArr);
       createMarker();
     }
@@ -90,3 +112,13 @@ const App = () => {
 };
 
 export default App;
+
+function OverlayComponent() {
+  return (
+    <div class="label">
+      <span class="left"></span>
+      <span class="center">위코드!!!!</span>
+      <span class="right"></span>
+    </div>
+  );
+}
